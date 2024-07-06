d940f63f28733a7a323dee80d778431873d4944a
Lily <56913305+Memez4Life7@users.noreply.github.com>
2024-07-06 13:26:07 +0200

    fnf page update

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,5 +1,28 @@
 setTheme(localStorage.getItem('theme'));
 
+function dialogClickHandler(e, callback) {
+    if (e.target.tagName !== 'DIALOG') //This prevents issues with forms
+        return;
+
+    const rect = e.target.getBoundingClientRect();
+
+    const clickedInDialog = (
+        rect.top <= e.clientY &&
+        e.clientY <= rect.top + rect.height &&
+        rect.left <= e.clientX &&
+        e.clientX <= rect.left + rect.width
+    );
+
+    if (clickedInDialog === false)
+        callback();
+}
+
+document.addEventListener("DOMContentLoaded", function(arg) {
+		document.getElementById("SettingsDialog").addEventListener('click', function(event) {
+        dialogClickHandler(event, toggleSettings);
+    });
+});
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -30,11 +53,13 @@ function setTheme(theme) {
         document.documentElement.style.setProperty('--menu-bg-color', '#111111');
         document.documentElement.style.setProperty('--text-color', '#fff');
         document.documentElement.style.setProperty('--text-highlight', '#FFA8D9');
+		document.documentElement.style.setProperty('--gradient', 'linear-gradient(to bottom right, rgba(119,81,109,1) 25%, rgba(200,134,184,1) 100%)');
     }
     if (theme == 'Blossom') {
         document.documentElement.style.setProperty('--bg-color', '#fff');
         document.documentElement.style.setProperty('--menu-bg-color', '#EEEEEE');
         document.documentElement.style.setProperty('--text-color', '#000');
         document.documentElement.style.setProperty('--text-highlight', '#FF66BF');
+		document.documentElement.style.setProperty('--gradient', 'linear-gradient(90deg, rgba(214,149,198,1) 25%, rgba(255,173,237,1) 100%)');
     }
 }
